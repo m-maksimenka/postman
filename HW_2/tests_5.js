20e63b7c5f95f45b7abcc535d45ae6fda82b940b
m-maksimenka <maryna-maksimenka@protonmail.com>
2021-09-19 01:40:57 +0300

    add minor fixes to tests_5.js

--- HW_2/tests_5.js
+++ HW_2/tests_5.js
@@ -4,57 +4,57 @@ pm.test("Status code is 200", function () {
 });
 
 // 2
-pm.test("JSON schema is OK", function() {
-    const schema = {
-        "type": "object",
-        "properties": {
-            "age": {
-                "type": "string"
-            },
-            "family": {
-                "type": "object",
-                "properties": {
-                    "children": {
-                        "type": "array",
-                        "items": [
-                            {
-                                "type": "array",
-                                "items": [
-                                    {
-                                        "type": "string"
-                                    },
-                                    {
-                                        "type": "integer"
-                                    }
-                                ]
-                            },
-                            {
-                                "type": "array",
-                                "items": [
-                                    {
-                                        "type": "string"
-                                    },
-                                    {
-                                        "type": "integer"
-                                    }
-                                ]
-                            }
-
-                        ]
-                    },
-                    "u_salary_1_5_year": {
-                        "type": "integer"
-                    }
+const schema = {
+    "type": "object",
+    "properties": {
+        "age": {
+            "type": "string"
+        },
+        "family": {
+            "type": "object",
+            "properties": {
+                "children": {
+                    "type": "array",
+                    "items": [
+                        {
+                            "type": "array",
+                            "items": [
+                                {
+                                    "type": "string"
+                                },
+                                {
+                                    "type": "integer"
+                                }
+                            ]
+                        },
+                        {
+                            "type": "array",
+                            "items": [
+                                {
+                                    "type": "string"
+                                },
+                                {
+                                    "type": "integer"
+                                }
+                            ]
+                        }
+                    ]
+                },
+                "u_salary_1_5_year": {
+                    "type": "integer"
                 }
-            },
-            "name": {
-                "type": "string"
-            },
-            "salary": {
-                "type": "integer"
             }
+        },
+        "name": {
+            "type": "string"
+        },
+        "salary": {
+            "type": "integer"
         }
     }
+}
+
+pm.test("JSON schema is correct", function() {
     pm.response.to.have.jsonSchema(schema)
 });
 
@@ -64,8 +64,9 @@ pm.test("Response property matches environment variable", function () {
 });
 
 // 4
+let requestData = request.data;
+let responseJson = pm.response.json();
+
 pm.test("Response property matches request property", function () {
-    const requestData = request.data;
-    const responseJson = pm.response.json();
     pm.expect(responseJson.age).to.eql(requestData.age);
 });
