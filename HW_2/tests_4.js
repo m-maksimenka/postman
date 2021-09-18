f50f0b334f6b9bea09f99bfd75f0d5e582de31e1
m-maksimenka <maryna-maksimenka@protonmail.com>
2021-09-19 01:34:07 +0300

    add minor fixes to tests_4.js

--- HW_2/tests_4.js
+++ HW_2/tests_4.js
@@ -4,34 +4,36 @@ pm.test("Status code is 200", function () {
 });
 
 // 2
-pm.test("JSON schema is OK", function() {
-    const schema = {
-        "type": "object",
-        "properties": {
-            "age": {
-                "type": "integer"
-            },
-            "daily_food": {
-                "type": "number"
-            },
-            "daily_sleep": {
-                "type": "number"
-            },
-            "name": {
-                "type": "string"
-            }
+const schema = {
+    "type": "object",
+    "properties": {
+        "age": {
+            "type": "integer"
+        },
+        "daily_food": {
+            "type": "number"
+        },
+        "daily_sleep": {
+            "type": "number"
+        },
+        "name": {
+            "type": "string"
         }
     }
+}
+
+pm.test("JSON schema is correct", function() {
     pm.response.to.have.jsonSchema(schema);
 });
 
 // 3
+let requestData = request.data;
+let responseJson = pm.response.json();
+let weight = requestData.weight;
+let daily_food = responseJson.daily_food;
+let daily_sleep = responseJson.daily_sleep;
+
 pm.test("Multiplication result is correct", function() {
-    const requestData = request.data;
-    const responseJson = pm.response.json();
-    let weight = requestData.weight;
-    let daily_food = responseJson.daily_food;
-    let daily_sleep = responseJson.daily_sleep;
     pm.expect(daily_food).to.eql(weight * 0.012);
     pm.expect(daily_sleep).to.eql(weight * 2.5);
 });
