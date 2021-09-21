7c49befdd4f882941ca778662408787b58aef49e
m-maksimenka <maryna-maksimenka@protonmail.com>
2021-09-21 19:57:37 +0300

    add fixes to tests_2.js

--- HW_2/tests_2.js
+++ HW_2/tests_2.js
@@ -30,7 +30,12 @@ const schema = {
                 "u_salary_1_5_year": {
                     "type": "integer"
                 }
-            }
+            },
+            "required": [
+                "u_age",
+                "u_name",
+                "u_salary_1_5_year"
+            ]
         },
         "qa_salary_after_12_months": {
             "type": "integer"
@@ -41,7 +46,13 @@ const schema = {
         "start_qa_salary": {
             "type": "integer"
         }
-    }
+    },
+    "required": [
+        "person",
+        "qa_salary_after_12_months",
+        "qa_salary_after_6_months",
+        "start_qa_salary"
+    ]
 }
 
 pm.test("JSON schema is correct", function() {
@@ -49,14 +60,13 @@ pm.test("JSON schema is correct", function() {
 });
 
 // 3
-let requestData = JSON.parse(request.data);
-let responseJson = pm.response.json();
-let salary = requestData.salary;
-let qa_salary_after_6_months = responseJson.qa_salary_after_6_months;
-let qa_salary_after_12_months = responseJson.qa_salary_after_12_months;
-let u_salary_1_5_year = responseJson.person.u_salary_1_5_year;
-
 pm.test("Multiplication result is correct", function() {
+    let requestData = JSON.parse(request.data);
+    let responseJson = pm.response.json();
+    let salary = requestData.salary;
+    let qa_salary_after_6_months = responseJson.qa_salary_after_6_months;
+    let qa_salary_after_12_months = responseJson.qa_salary_after_12_months;
+    let u_salary_1_5_year = responseJson.person.u_salary_1_5_year;
     pm.expect(qa_salary_after_6_months).to.eql(salary * 2);
     pm.expect(qa_salary_after_12_months).to.eql(salary * 2.9);
     pm.expect(u_salary_1_5_year).to.eql(salary * 4);
