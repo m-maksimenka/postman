ed5e16970de6b4af7ebde5937a098386634097e2
m-maksimenka <maryna-maksimenka@protonmail.com>
2021-09-23 21:12:18 +0300

    refactor test scripts

--- HW_2/tests_6.js
+++ HW_2/tests_6.js
@@ -1,3 +1,3 @@
-let responseJson = pm.response.json();
-let randomCurrency = responseJson[Math.floor(Math.random() * responseJson.length)];
+let jsonData = pm.response.json();
+let randomCurrency = jsonData[Math.floor(Math.random() * jsonData.length)];
 pm.environment.set("random_id", randomCurrency.Cur_ID);
